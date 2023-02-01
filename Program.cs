8d832a585f15e9e6b49381f5723cda68aae3ec53
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2023-02-01 20:09:08 +0200

    Add a lot project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp.GitExample;
+
+using System;
+
+internal class Program
+{
+    internal static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
